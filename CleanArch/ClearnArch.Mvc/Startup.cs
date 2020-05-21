1005b98589df8641645fefe616d54bd76d8c5119
c.p.kondas <c.p.kondas@leidos.com>
2020-05-21 14:42:32 -0400

    Configure presentation layer for dependency container

--- CleanArch/ClearnArch.Mvc/Startup.cs
+++ CleanArch/ClearnArch.Mvc/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
 using ClearnArch.Mvc.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CleanArch.Infra.Data.Context;
 
 namespace ClearnArch.Mvc
 {
@@ -39,6 +34,8 @@ namespace ClearnArch.Mvc
             {
                 options.UseSqlServer(Configuration.GetConnectionString("UniversityDbConnection"));
             });
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -70,6 +67,11 @@ namespace ClearnArch.Mvc
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
