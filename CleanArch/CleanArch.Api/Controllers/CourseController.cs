70ce69da3468a31d962509040be8822a5794dbab
c.p.kondas <c.p.kondas@leidos.com>
2020-05-22 13:46:17 -0400

    Add Api Project and rename controller

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+    }
+}
